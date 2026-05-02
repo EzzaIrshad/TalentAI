0ab19a4944dc6944fd0592b22160dd1ed4b94895
Ezza Irshad <ezzairshad19@gmail.com>
2026-05-02 16:34:47 +0500

    setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/TalentAI",
+  assetPrefix: "/TalentAI/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
