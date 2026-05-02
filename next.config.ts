66ff9bbe642d28b945f65c28b856aa554875fc99
Ezza Irshad <ezzairshad19@gmail.com>
2026-05-02 17:07:11 +0500

    Configuration file fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/TalentAI",
-  assetPrefix: "/TalentAI/",
   images: {
     unoptimized: true,
   },
